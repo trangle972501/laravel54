3bff19d449342ba3cdecee37b35c3a18dc994c41
ngodinhtrongpnv17 <trongpnv17@gmail.com>
2017-06-08 11:36:17 +0700

    fodder car

---
+++ resources/views/car/detail.blade.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <title>Welcome to Product Detail Page</title>
+   <meta name="description" content="">
+   <meta name="author" content="">
+   <meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
+   <!-- Mobile Specific Meta -->
+   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <!-- Latest compiled and minified CSS -->
+   <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+   <!-- Main Stylesheets -->
+   <link href="{{asset('css/product-detail.css')}}" rel="stylesheet"  id="menucss">
+   <link href="{{asset('css/product-2.css')}}"  rel="stylesheet" type="text/css">
+   <!-- Favicon Icon -->
+   <link rel="shortcut icon" href="images/icon.png">
+   <!-- Google web font ---->
+   <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+   <!-- Awesome font icon ---->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
+  </head>
+  <body>
+   <!-- Space top -->
+   <div class="space-top">
+   </div>
+   <div class="pld">
+         <!--[if lt IE 8]>
+         <!-- Product Simple Area Start -->
+         <div class="product-simple-area">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-6 col-sm-6">
+                     <div class="single-product-image-inner">
+                        <!-- Tab panes -->
+                        <div class="tab-content">
+                           <div role="tabpanel" class="tab-pane active" id="one">
+                              <img src="http://lh5.googleusercontent.com/-nq-SuSA1x5c/VGWBhIkI5RI/AAAAAAAAFOA/4zm2Dj1Wc3c/w1200-h630-p-k-no-nu/Z1000-Xanh-2015.jpg" alt="" class="img-thumbnail"> 
+                           </div>
+                           <div role="tabpanel" class="tab-pane" id="two">
+                              <img src="https://www.asphaltandrubber.com/wp-content/gallery/2014-kawasaki-z1000-video-leak/2014-kawasaki-z1000-video-leak-25.jpg" alt="" class="img-thumbnail">
+                           </div>
+                           <div role="tabpanel" class="tab-pane" id="three">
+                              <img src="https://cdn.motosaigon.vn/wp-content/uploads/2016/05/8-266.jpg" alt=""" class="img-thumbnail">
+                           </div>
+                        </div>
+                        <!-- Nav tabs -->
+                        <ul class="product-tabs" role="tablist">
+                           <li role="presentation" class="active"><a href="#one" aria-controls="one" role="tab" data-toggle="tab">
+                              <img src="http://lh5.googleusercontent.com/-nq-SuSA1x5c/VGWBhIkI5RI/AAAAAAAAFOA/4zm2Dj1Wc3c/w1200-h630-p-k-no-nu/Z1000-Xanh-2015.jpg" class="img-thumbnail" alt="Cinque Terre" width="150" height="200">
+                           </a></li>
+                           <li role="presentation"><a href="#two" aria-controls="two" role="tab" data-toggle="tab">  <img src="https://www.asphaltandrubber.com/wp-content/gallery/2014-kawasaki-z1000-video-leak/2014-kawasaki-z1000-video-leak-25.jpg" class="img-thumbnail" alt="Cinque Terre" width="150" height="200"></a></li>
+                           <li role="presentation"><a href="#three" aria-controls="three" role="tab" data-toggle="tab"> <img src="https://i.ytimg.com/vi/4U8nuCGoc84/maxresdefault.jpg" class="img-thumbnail" alt="Cinque Terre" width="150" height="200"></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="clearfix visible-xs"></div>
+                  <div class="col-md-offset-1 col-md-5 col-sm-6">
+                     <div class="single-product-details">
+                        <h2>Moto Z1000</h2>
+                     
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, pariatur iure. Temporibus accusantium animi ducimus voluptatum, nihil at dolore reprehenderit hic itaque ab quas ad ullam harum eius laborum, in nostrum totam et, placeat ratione eum. Reprehenderit, esse. Culpa, iure! </p>
+                        <div class="single-product-price">
+                           <h2>Giá: </h2><h2 style="color: red; padding-left: 15px">$50.66</h2>
+                        </div>
+                        <span >Tình trạng xe :</span>
+                        <p class="single-shop-select">
+                           <br>
+                           <label>Số lượng xe đặt</label>
+                           <select>
+                              <option>1</option>
+                              <option>2</option>
+                              <option>3</option>
+                              <option>4</option>
+                              <option>5</option>
+                              <option>6</option>
+                           </select>
+                        </p>
+                        <div class="product-attributes clearfix">
+                           <span>
+                              <a class="cart-btn btn-success" href="#" style="padding-right: 20px">
+                                 <span><i class="fa fa-bolt"></i> Đặt ngay</span>
+                              </a>
+                           </span>
+                        </div>
+                        <hr>
+                        <div class="single-product-categories">
+                           <label>Categories:</label>
+                           <span>Moto</span>
+                           <div class="clear"></div>
+                           <label>Services:</label>
+                           <span>Đồ bảo hộ</span>
+                           <div class="clear"></div>
+                           <label></label>
+                           
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-md-9">
+                     <div class="p-details-tab-content section-padding-2">
+                        <div class="p-details-tab">
+                           <ul class="p-details-nav-tab" role="tablist">
+                              <li role="presentation" class="active">
+                                 <a href="#more-info" aria-controls="more-info" role="tab" data-toggle="tab">Chi tiết xe</a>
+                              </li>
+                              <li role="presentation"><a href="#review" aria-controls="data" role="tab" data-toggle="tab">Nhận xét</a></li>
+                              
+                           </ul>
+                        </div>
+                        <div class="clearfix"></div>
+                        <div class="tab-content review">
+                           <div role="tabpanel" class="tab-pane active" id="more-info">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, ea ipsum provident dolore nisi impedit ut repudiandae rem veritatis eum nostrum hic blanditiis, modi fugit sed, explicabo pariatur sequi ad consequuntur temporibus fugiat saepe incidunt facilis. Ad inventore quis nulla maxime, cum quasi sit fuga, quae recusandae quod dolore sapiente doloremque dignissimos rerum consectetur eligendi saepe voluptates voluptatibus ipsam dolores. Dolorem excepturi quia fugiat iure tempore nisi id deserunt nemo fuga, sed illo adipisci, sequi ipsa, eum possimus quo neque. Distinctio consequatur, pariatur, tempore voluptatum placeat aspernatur qui ad quae non ipsum suscipit, porro cum, voluptates asperiores provident vel eum.</p>
+                           </div>
+                           <div role="tabpanel" class="tab-pane" id="review">
+                              <form class="form-horizontal">
+                                 <div id="review"></div>
+                                 <h2>Nhận xét</h2>
+                                 <div class="form-group required">
+                                    <div class="col-sm-12">
+                                       <label class="control-label" for="input-name">Your Name</label>
+                                       <input type="text" name="name" value="" id="input-name" class="form-control">
+                                    </div>
+                                 </div>
+                                 <div class="form-group required">
+                                    <div class="col-sm-12">
+                                       <label class="control-label" for="input-review">Your Message</label>
+                                       <textarea name="text" rows="5" id="input-review" class="form-control"></textarea>
+                                    </div>
+                                 </div>
+                                 <div class="form-group required">
+                                    
+                                 </div>
+                                 <div class="buttons clearfix">
+                                    <div class="pull-right">
+                                       <button type="button" id="button-review" data-loading-text="Loading..." class="btn btn-primary">Send</button>
+                                    </div>
+                                 </div>
+                              </form>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- Product Simple Area End -->
+         <!--Related Product Area Start-->
+         <div class="featured-product-area section-padding-2">
+            <div class="container">
+               <div class="row">
+                  <div class="section-title">
+                     <h2>Xe tương tự</h2>
+                  </div>
+                  <div class="col-lg-3 col-sm-6  hero-feature text-center">
+                     <!-- Thumbnail Images -->
+                     <div class="thumbnail">
+                        <a href="#" class="link-p"> <img src="http://cdn1.autodaily.vn/2013/12/_DSC0242.jpg" alt="" class="img-thumbnail"> </a>
+                        <div class="like-heart"><a  data-placement="left"> <i class="fa fa-heart-o" aria-hidden="true"></i></a></div>
+                        <!-- Offer Tag-->
+                        <span class="offer">Hot</span>
+                        <div class="caption prod-caption">
+                           <span class="brand animated fadeInRightShort">Moto 175cc</span>
+                           <div class="pro-name"><a href="#" class="animated fadeInRightShort">XE Moto phân khối Đà Nẵng</a></div>
+                           <p>
+                              <!-- Price & Button -->
+                              <div class="btn-group"> <a href="#" class="btn btn-default">$ 500.000</a> <a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Đặt</a> </div>
+                           </p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6  hero-feature text-center">
+                     <!-- Thumbnail Images -->
+                     <div class="thumbnail">
+                        <a href="#" class="link-p"> <img src="http://cdn1.autodaily.vn/2013/12/_DSC0242.jpg" alt="" class="img-thumbnail"> </a>
+                        <div class="like-heart"><a  data-placement="left"> <i class="fa fa-heart-o" aria-hidden="true"></i></a></div>
+                        <!-- Offer Tag-->
+                        <span class="offer">Sale</span>
+                        <div class="caption prod-caption">
+                           <span class="brand animated fadeInRightShort">Moto 175cc</span>
+                           <div class="pro-name"><a href="#" class="animated fadeInRightShort">XE Moto phân khối Đà Nẵng</a></div>
+                           <p>
+                              <!-- Price & Button -->
+                              <div class="btn-group"> <a href="#" class="btn btn-default">$ 500.000</a> <a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Đặt</a> </div>
+                           </p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6  hero-feature text-center">
+                     <!-- Thumbnail Images -->
+                     <div class="thumbnail">
+                        <a href="#" class="link-p"> <img src="http://cdn1.autodaily.vn/2013/12/_DSC0242.jpg" alt="" class="img-thumbnail"> </a>
+                        <div class="like-heart"><a  data-placement="left"> <i class="fa fa-heart-o" aria-hidden="true"></i></a></div>
+                        <!-- Offer Tag-->
+                        <span class="offer">Sale</span>
+                        <div class="caption prod-caption">
+                           <span class="brand animated fadeInRightShort">Moto 175cc</span>
+                           <div class="pro-name"><a href="#" class="animated fadeInRightShort">XE Moto phân khối Đà Nẵng</a></div>
+                           <p>
+                              <!-- Price & Button -->
+                              <div class="btn-group"> <a href="#" class="btn btn-default">$ 500.000</a> <a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Đặt</a> </div>
+                           </p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6  hero-feature text-center">
+                     <!-- Thumbnail Images -->
+                     <div class="thumbnail">
+                        <a href="#" class="link-p"> <img src="http://cdn1.autodaily.vn/2013/12/_DSC0242.jpg" alt="" class="img-thumbnail"> </a>
+                        <div class="like-heart"><a  data-placement="left"> <i class="fa fa-heart-o" aria-hidden="true"></i></a></div>
+                        <!-- Offer Tag-->
+                        <span class="offer">Sale</span>
+                        <div class="caption prod-caption">
+                           <span class="brand animated fadeInRightShort">Moto 175cc</span>
+                           <div class="pro-name"><a href="#" class="animated fadeInRightShort">XE Moto phân khối Đà Nẵng</a></div>
+                           <p>
+                              <!-- Price & Button -->
+                              <div class="btn-group"> <a href="#" class="btn btn-default">$ 500.000</a> <a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Đặt</a> </div>
+                           </p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+           <!-- Space bottom -->
+            <div class="space-bottom"><i class="fa fa-bullseye" aria-hidden="true"></i> Footer Section <i class="fa fa-bullseye" aria-hidden="true"></i></div>
+            <!-- jQuery JavaScript plugins -->
+            <script src="{{asset('js/jquery-1.12.4.min.js')}}" type="text/javascript"></script>
+            <!-- Bootstrap JavaScript plugins -->
+            <script src="{{asset('js/bootstrap.js')" type="text/javascript"></script>
+            <!-- owl.carousel js -->
+            <script src="{{asset('js/owl.carousel.min.js')" type="text/javascript"></script>
+         </body>
+         </html>
